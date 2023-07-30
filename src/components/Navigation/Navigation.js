431125a72a285fe70182d50f6ae60d86097feeb3
tatsenko-m <tatsenko.m@yandex.ru>
2023-07-31 02:03:51 +0300

    feat: доделать верстку Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,26 +5,28 @@ import AccountLink from '../AccountLink/AccountLink';
 const Navigation = ({ loggedIn }) => {
   return (
     <nav className="navigation">
-        {loggedIn ? (
-          <>
-            <ul className="navigation__links">
-              <li className="navigation__item">
-                <NavLink to="/movies" className={({isActive}) => `navigation__link ${isActive ? "navigation__link_active" : ""}`}>Фильмы</NavLink>
-              </li>
-              <li className="navigation__item">
-                <NavLink to="/saved-movies" className={({isActive}) => `navigation__link ${isActive ? "navigation__link_active" : ""}`}>Сохраненные фильмы</NavLink>
-              </li>
-              <li className="navigation__item">
-                <AccountLink />
-              </li>
-            </ul>
-          </>
-        ) : (
-          <>
-            <Link to="/signup">Регистрация</Link>
-            <Link to="/signin">Войти</Link>
-          </>
-        )}
+      {loggedIn ? (
+        <ul className="navigation__links">
+          <li className="navigation__item">
+            <NavLink to="/movies" className={({isActive}) => `navigation__link ${isActive ? "navigation__link_active" : ""}`}>Фильмы</NavLink>
+          </li>
+          <li className="navigation__item">
+            <NavLink to="/saved-movies" className={({isActive}) => `navigation__link ${isActive ? "navigation__link_active" : ""}`}>Сохраненные фильмы</NavLink>
+          </li>
+          <li className="navigation__item">
+            <AccountLink />
+          </li>
+        </ul>
+      ) : (
+        <ul className="navigation__links navigation__links_unauth">
+          <li className="navigation__item">
+            <Link to="/signup" className="navigation__signup-link">Регистрация</Link>
+          </li>
+          <li className="navigation__item">
+            <Link to="/signin" className="navigation__signin-link">Войти</Link>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 };

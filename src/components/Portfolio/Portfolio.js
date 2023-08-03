255450fd530922103064b1d340749f1be77c5bfb
tatsenko-m <tatsenko.m@yandex.ru>
2023-08-03 15:35:47 +0300

    feat: сверстать Portfolio для desktop

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,23 +5,23 @@ const Portfolio = () => {
     <section className="portfolio">
       <h2 className="portfolio__heading">Портфолио</h2>
       <nav>
-        <ul>
+        <ul className="portfolio__links">
           <li className="portfolio__item">
             <Link to="https://github.com/tatsenko-m/how-to-learn" target="_blank" className="portfolio__link">
               <p className="portfolio__link-text">Статичный сайт</p>
-              <div className="portfolio__link-icon"></div>
+              <span className="portfolio__arrow-symbol">&#8599;</span>
             </Link>
           </li>
           <li className="portfolio__item">
             <Link to="https://github.com/tatsenko-m/russian-travel" target="_blank" className="portfolio__link">
               <p className="portfolio__link-text">Адаптивный сайт</p>
-              <div className="portfolio__link-icon"></div>
+              <span className="portfolio__arrow-symbol">&#8599;</span>
             </Link>
           </li>
           <li className="portfolio__item">
             <Link to="https://github.com/tatsenko-m/react-mesto-api-full-gha" target="_blank" className="portfolio__link">
               <p className="portfolio__link-text">Одностраничное приложение</p>
-              <div className="portfolio__link-icon"></div>
+              <span className="portfolio__arrow-symbol">&#8599;</span>
             </Link>
           </li>
         </ul>

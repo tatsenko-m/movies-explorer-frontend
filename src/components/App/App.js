828b1a78a5ac630a79c9d63f37b257e7ed645baa
tatsenko-m <tatsenko.m@yandex.ru>
2023-08-04 23:32:50 +0300

    feat: добавить preloader в проект

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 
-import Header from '../Header/Header';
-import Main from '../Main/Main';
-import Footer from '../Footer/Footer';
+import Preloader from "../Preloader/Preloader";
+import Header from "../Header/Header";
+import Main from "../Main/Main";
+import Footer from "../Footer/Footer";
 
 function App() {
   const [loggedIn, setLoggedIn] = React.useState(false);
+  const [isLoading, setIsLoading] = React.useState(false);
 
   return (
     <>
       <Header loggedIn={loggedIn} />
-      <Main />
+      <Routes>
+        <Route path="/" element={isLoading ? <Preloader /> : <Main />} />
+      </Routes>
       <Footer />
     </>
   );

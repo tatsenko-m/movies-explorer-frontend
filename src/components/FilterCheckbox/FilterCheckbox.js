d2ea92f76cfc4f3915d32d235f815d8d7bd1b13a
tatsenko-m <tatsenko.m@yandex.ru>
2023-08-14 18:33:48 +0300

    feat: реализовать начальную версию поиска и поправить стили карточки фильма

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const FilterCheckbox = () => {
+const FilterCheckbox = ({ isShortMovies, onShortMoviesCheck }) => {
   return (
     <div className="filter">
       <label className="filter__label">
-        <input type="checkbox" className="filter__checkbox" />
+        <input type="checkbox" className="filter__checkbox" onChange={onShortMoviesCheck} checked={isShortMovies} />
         <span className="filter__slider" />
       </label>
       <p className="filter__caption">Короткометражки</p>
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -2,15 +2,26 @@ import React from 'react';
 
 import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 
-const SearchForm = () => {
+const SearchForm = ({ isShortMovies, onShortMoviesCheck, onSearchMovies }) => {
+  const [searchFormInput, setSearchFormInput] = React.useState('');
+
+  function handleInputChange(evt) {
+    setSearchFormInput(evt.target.value);
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    onSearchMovies(searchFormInput);
+  }
+
   return (
     <section className="search">
-      <form className="search__form">
+      <form className="search__form" onSubmit={handleSubmit}>
         <div className="search__search-bar">
-          <input className="search__form-input" type="text" placeholder="Фильм" />
+          <input className="search__form-input" type="text" placeholder="Фильм" onChange={handleInputChange} />
           <button className="search__form-button" type="submit">Поиск</button>
         </div>
-        <FilterCheckbox />
+        <FilterCheckbox isShortMovies={isShortMovies} onShortMoviesCheck={onShortMoviesCheck} />
       </form>
     </section>
   );
